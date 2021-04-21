634233efc7f7278ffb471ad140f00b88d41c05eb
KAthousand <KAthousand@gmail.com>
2021-04-20 21:21:42 -0400

    added autoplay to slideshow, fixed last slide left

--- client/src/components/CarouselSlide/CarouselSlide.jsx
+++ client/src/components/CarouselSlide/CarouselSlide.jsx
@@ -8,7 +8,7 @@ function CarouselSlide(props) {
   let active;
   if (slideIndex === idx) {
     active = 'slide'
-  } else if (slideIndex + 1 === idx || (slideIndex === myArr.length-1 && idx === 0)) {
+  } else if (slideIndex + 1 === idx || (slideIndex === myArr.length - 1 && idx === 0)) {
     active = 'slide-transition-left'
   } else if (slideIndex - 1 === idx || (slideIndex === 0 && idx === myArr.length-1)) {
     active = 'slide-transition-right'
--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import CarouselSlide from '../CarouselSlide/CarouselSlide';
 import "./Slider.css"
 
@@ -6,24 +6,36 @@ function Slider(props) {
 
   const [backgroundColor, setBackgroundColor] = useState(false)
   const [slideIndex, setSlideIndex] = useState(0);
+  const [timer, setTimer] = useState(0)
+  const [play, setPlay] = useState(true)
   const myArr = ['blue', 'purple', 'green', 'orange']
   // const myArr = [<CarouselSlide content={'blue'}/>,<CarouselSlide content={'purple'} />,<CarouselSlide content={'green'}/>,<CarouselSlide content={'orange'}/>]
-  const goRight = () => {
+  const goLeft = () => {
     if (slideIndex < myArr.length-1) {
       setSlideIndex(slideIndex + 1)
       setTimeout(setBackgroundColor(myArr[slideIndex], 250))
     } else {
       setSlideIndex(0);
+      setTimeout(setBackgroundColor(myArr[slideIndex], 250))
     }
   };
-  const goLeft = () => {
+  const goRight = () => {
     if (slideIndex > 0) {
       setSlideIndex(slideIndex - 1)
       setTimeout(setBackgroundColor(myArr[slideIndex], 250))
     } else {
       setSlideIndex(myArr.length - 1);
+      setTimeout(setBackgroundColor(myArr[slideIndex], 250))
     };
   }
+
+  useEffect(() => {
+    if (play) {
+      setTimeout(goRight, 3000)
+    } else {
+      clearTimeout()
+    }
+  }, [play, goRight])
 
   return (
     <div className='slider'>
@@ -31,16 +43,19 @@ function Slider(props) {
         <button id='goLeft' onClick={goLeft}>
           left
         </button>
+        <button onClick={()=>setPlay(!play)}>
+          {play ? "pause": "play"}
+        </button>
         <button id='goLeft' onClick={goRight}>
-            right
+          right
         </button>
       </div>
       <div className='slider-content' style={{backgroundColor: backgroundColor}}>
         {myArr.map((elem, idx) => {
           if (idx === 0) {
-            return <CarouselSlide content={elem} slideIndex={slideIndex} idx={idx} myArr={myArr}/>
+            return <CarouselSlide content={elem} slideIndex={slideIndex} idx={idx} myArr={myArr} key={idx+1}/>
           } else {
-            return <CarouselSlide content={elem} slideIndex={slideIndex} idx={idx} myArr={myArr}/>
+            return <CarouselSlide content={elem} slideIndex={slideIndex} idx={idx} myArr={myArr} key={idx+1}/>
           }
         })}
         </div>
