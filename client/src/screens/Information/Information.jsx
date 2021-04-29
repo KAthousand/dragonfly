2096fb475da79123e78fdada6d29422193e19513
KAthousand <KAthousand@gmail.com>
2021-04-28 22:09:08 -0400

    info container styling updates

--- client/src/screens/Information/Information.jsx
+++ client/src/screens/Information/Information.jsx
@@ -6,16 +6,20 @@ function Information(props) {
     <div className='info-container'>
       <div className='info-content'>
         <div className='info'>
-        <div className='info-title'></div>
-          <p>1234 Address St,</p>
-          <p>Detroit, MI 48221</p>
-          <p>(555) 555-5555</p>
+          <div className='info-title'></div>
+          <div className='info-location'>
+            <h4>Location</h4>
+            <p>1234 Address St,</p>
+            <p>Detroit, MI 48221</p>
+            <p>(555) 555-5555</p>
+          </div>
           <br />
           <div className='info-hours'>
+            <h4>Hours</h4>
             <p>Monday to Thursday</p>
-            <h3>5pm - 12am</h3>
+            <h5>5pm - 12am</h5>
             <p>Friday to Sunday</p>
-            <h3>4pm - 2am</h3>
+            <h5>4pm - 2am</h5>
           </div>
         </div>
       </div>
